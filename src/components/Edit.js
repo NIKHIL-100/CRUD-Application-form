ac737bd0b071ddecc0e04cc5d81f7316a9770d88
Nikhil Chavan <chavannikhil1007@gmail.com>
2022-10-19 22:37:42 +0530

    secon commit

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Empolyees from "./Employees";
-import { v4 as uuid } from "uuid";
-import { Link, useNavigate } from "react-router-dom";
+// import { v4 as uuid } from "uuid";
+import {  useNavigate } from "react-router-dom";
 
 const Edit = () => {
 
