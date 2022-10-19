6715d617a02e5307ddccbb7f7f3410a7845bc4f6
Nikhil Chavan <chavannikhil1007@gmail.com>
2022-10-19 22:30:56 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,20 @@
+const Empolyees = [
+    {
+        id:"1",
+        Name:"Mahesh Kharat",
+        Age:"22"
+    },
+    {
+        id:"2",
+        Name:"Onkar Jog",
+        Age:"23"
+    },
+    {
+        id:"3",
+        Name:"Siddhant Patil",
+        Age:"24"
+    }
+]
+
+
+export default Empolyees;
